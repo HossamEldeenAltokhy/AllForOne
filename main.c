b0af5debd840e24343e69f4c301e761028433a2e
Nihalkhaled <nihal0khaled@gmail.com>
2020-03-27 19:20:44 +0200

    first commit_Nihal

--- main.c
+++ main.c
@@ -1,33 +1,36 @@
 #include <stdio.h>
 #include <stdlib.h>
-#include <stdbool.h>
-#include "calc.h"
-#include "swap.h"
-#include "maxofarr.h"
-#include "maxzero.h"
-#include "swp_arr.h"
-#include "cap.h"
-#include "cap1st.h"
-#include "dupcheck.h"
-#include "swap.h"
-#include "bubblesort.h"
-#include "selectsort.h"
-#include "arrprint.h"
-#include "swapbitwise.h"
-#include "stack.h"
-#include "draw.h"
-#include <string.h>
-#include "rotatebinary.h"
-int main()
-{
-    int x[5]={44,33,2,111,6};
 
-    int y=4;
-    int* yptr =(int*)malloc(sizeof(y));
-    bubblesort(&x[1],y);
-    arrprint(x,5);
-    printf("%d\n",y);
-    free(yptr);
-
+void update(int *a,int *b) {
+int temp=*a;
+*a=*a+*b;
+*b=abs(temp-*b);
 }
 
+int main() {
+    int a, b;
+    int *pa = &a, *pb = &b;
+//
+//    scanf("%d %d", &a, &b);
+//    update(pa, pb);
+//    printf("%d\n%d", a, b);
+// char ch;
+//string s;
+// string sen;
+//scanf("%c", &ch);
+//scanf("%s", &s);
+//scanf("%[^\n]%*c", &sen);
+//    //printf("C\nLanguage\nWelcome To C!!");
+//printf("%c\n%s\n%s",ch,s,sen);
+//    return 0;
+
+char ch;            //use this code
+char s[100];
+char s1[122];
+scanf("%c%*c",&ch);
+scanf("%s%*c",&s);
+scanf("%[^\n]",&s1);
+printf("%c\n",ch);
+printf("%s\n",s);
+printf("%s",s1);
+return 0;}
